8c53a675a174c5df5263d989a0f00a2a3503ee35
tom-fitz <16616192+tom-fitz@users.noreply.github.com>
2024-04-15 16:10:29 -0600

    creating multiple leagues!

--- league/league.go
+++ league/league.go
@@ -2,15 +2,30 @@ package league
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func (h *Handler) createLeagues(w http.ResponseWriter, r *http.Request) {
-	var requestBody map[string]interface{}
+	var requestBody []map[string]interface{}
+
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
 		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
 		return
 	}
-	h.logger.Printf("Received POST request with JSON body: %v", requestBody)
+
+	var ids []int
+	query := "INSERT INTO score_keep_db.public.leagues (name, level) VALUES ($1, $2) RETURNING id"
+	for _, league := range requestBody {
+		var id int
+		err := h.db.QueryRow(query, league["name"], league["level"]).Scan(&id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to insert data into database: %v", err), http.StatusInternalServerError)
+			return
+		}
+		ids = append(ids, id)
+	}
+
+	json.NewEncoder(w).Encode(map[string][]int{"ids": ids})
 }
