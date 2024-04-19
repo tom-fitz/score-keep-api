d53fbc0936b1210d73e275960bf409c3815568d4
tom-fitz <16616192+tom-fitz@users.noreply.github.com>
2024-04-19 13:16:06 -0600

    retrieving correct list of events. Was using actual id instead of the name of the calendar as the id. Doh.

--- calendar/routes.go
+++ calendar/routes.go
@@ -13,47 +13,17 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 }
 
 func (h *Handler) listEvents(w http.ResponseWriter, r *http.Request) {
-	//settings, err := h.gcp.Settings.List().Do()
-	// https://calendar.google.com/calendar/u/0?cid=dHBmaXR6NDJAZ21haWwuY29t
-	acl, err := h.gcp.Acl.List("primary").Do()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	h.logger.Printf("acl: %+v", acl)
-	for _, item := range acl.Items {
-		h.logger.Printf("acl 2: %+v", *item.Scope)
-	}
+	// ID of the calendar to access
+	calendarID := "tpfitz42@gmail.com"
 
-	//tokenInfo, err := h.gcp.Tokeninfo().Context(h.ctx).Do()
-	//if err != nil {
-	//	http.Error(w, fmt.Sprintf("Unable to get token info: %v", err), http.StatusInternalServerError)
-	//	return
-	//}
 	// Fetch events from the specified calendar
-	events, err := h.gcp.Events.List("primary").Context(h.ctx).Do()
+	events, err := h.gcp.Events.List(calendarID).Context(h.ctx).Do()
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Unable to retrieve events: %v", err), http.StatusInternalServerError)
 		return
 	}
 
-	// ID of the calendar to insert
-	calendarID := "dHBmaXR6NDJAZ21haWwuY29t"
-
-	// Create a new CalendarListEntry object with the specified ID
-	newCalendar := &calendar.CalendarListEntry{
-		Id: calendarID,
-	}
-
-	calListEntry, err := h.gcp.CalendarList.Insert(newCalendar).Context(h.ctx).Do()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Unable to insert calendar id: %s err: %v", "dHBmaXR6NDJAZ21haWwuY29t", err), http.StatusInternalServerError)
-		return
-	}
-
-	h.logger.Printf("calListEntry: %+v", calListEntry)
-
-	// Fetch a list of all calendars the user has access to
+	// Fetch a list of all calendars the service account has access to
 	calendars, err := h.gcp.CalendarList.List().Context(h.ctx).Do()
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Unable to retrieve calendar list: %v", err), http.StatusInternalServerError)
